26169cd9c3ac1657df6767e3765068a4a7727209
Kim Engse <error302@naver.com>
2024-02-22 12:50:53 +0900

    24.02.22 v2

--- 03/scripts/03.js
+++ 03/scripts/03.js
@@ -1,1 +1,6 @@
 // alert("안녕하세요");
+
+function handleClick(n) {
+//    document.querySelector("#msgArea").innerHTML = "<h2>Button " + n + " is selected</h2>"
+    document.querySelector("#msgArea").innerHTML = `<h2>Button ${n} is selected</h2>`
+}
