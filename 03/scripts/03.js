4f4934f5cbaac0b2366a2a654e62f128873bdef2
Kim Engse <error302@naver.com>
2024-02-22 12:59:43 +0900

    24.02.22 v3

--- 03/scripts/03.js
+++ 03/scripts/03.js
@@ -1,4 +1,8 @@
 // alert("안녕하세요");
+// 함수작성 2
+// const handleClick = (n) => {
+//     document.querySelector("#msgArea").innerHTML = `<h2>Button ${n} is selected</h2>`
+// }
 
 function handleClick(n) {
 //    document.querySelector("#msgArea").innerHTML = "<h2>Button " + n + " is selected</h2>"
