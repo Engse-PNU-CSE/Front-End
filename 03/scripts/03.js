35262efa05fd4f8ab6711f8e87185f9cfe6b3c5d
Kim Engse <error302@naver.com>
2024-02-23 14:49:59 +0900

    24.02.23 v1

--- 03/scripts/03.js
+++ 03/scripts/03.js
@@ -8,3 +8,16 @@ function handleClick(n) {
 //    document.querySelector("#msgArea").innerHTML = "<h2>Button " + n + " is selected</h2>"
     document.querySelector("#msgArea").innerHTML = `<h2>Button ${n} is selected</h2>`
 }
+
+//After DomTree making
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOMContentLoaded OK");
+    //create Button
+    const bt3 = document.createElement("button");
+    //add Button
+    document.querySelector("#btArea2").append(bt3);
+    //create Button Text
+    const bt3txt = document.createTextNode("Button 3");
+    //add Button text
+    bt3.appendChild(bt3txt);
+});
