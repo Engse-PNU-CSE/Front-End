d4d499c5aae284642e9bfe81feb8e89877cbac45
Kim Engse <error302@naver.com>
2024-02-26 17:38:03 +0900

    24.02.26 v3

--- 07/scripts/07.js
+++ 07/scripts/07.js
@@ -10,13 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const labels = document.querySelectorAll("label");
 
     sel1.addEventListener("change", () => {
-        handleChange(sel1, sel2, labels[0], labels[1]);
+        handleChange(sel1, sel2, labels[0], labels[1], txt1, txt2);
+ 
     });
     sel2.addEventListener("change", () => {
-        handleChange(sel2, sel1, labels[1], labels[0]);
+        handleChange(sel2, sel1, labels[1], labels[0], txt1, txt2);
+
     });
 
-    txt1.addEventListener("change", () => {
+    txt1.addEventListener("input", () => {
         if(sel1.value === "°C") {
             txt2.value = (txt1.value * 1.8) + 32;
         }
@@ -27,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-const handleChange = (s1, s2, l1, l2) => {
+const handleChange = (s1, s2, l1, l2, t1, t2) => {
     if(s1.value === "°C") {
         s2.value = "°F";
         l1.innerHTML = "°C";
@@ -38,4 +40,6 @@ const handleChange = (s1, s2, l1, l2) => {
         l1.innerHTML = "°F";
         l2.innerHTML = "°C";
     }
+    t1.value=t2.value='';
+    t1.focus();
 }
