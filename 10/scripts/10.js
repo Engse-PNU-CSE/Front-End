d9d5ddc0e000d2a0d889ff9612652b934660f932
Kim Engse <error302@naver.com>
2024-03-08 16:21:43 +0900

    24.0308 v2

--- 10/scripts/10.js
+++ 10/scripts/10.js
@@ -1,46 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const bts =  document.querySelector("section button")
-    bts.addEventListener("click", () => {
-        clickbts();
-    })
-  const numarea =  document.querySelector("section #numberArea")
+  const bts = document.querySelector("section button");
+  bts.addEventListener("click", () => {
+    clickbts();
+  });
+  const numarea = document.querySelector("section #numberArea");
 });
 const clickbts = () => {
-    let lottos = [];
-  while (lottos.length < 7) {
+  let lottos = [];
+  let numarea = document.querySelector("section div #numberArea");
+  //lottos.length = 0 <- 초기화랑 같은 효과
+  while (lottos.length < 8) {
     let lotto_number = Math.floor(Math.random() * 45 + 1);
     if (lotto_number in lottos) continue;
-    lottos.push(lotto_number);
+    else {
+      lottos.push(`<span class = "sp${parseInt(lotto_number / 10)}">${lotto_number}</span>`);
+      if(lottos.length==6) lottos.push(`<span class = "plus">+</span>`);
+    }
   }
-  let basic_number = lottos.slice(0, -1).sort();
-  let bouns_number = lottos.slice(-1)
-  
-//   for (let i = 1; i < 7; i++) {
-//     let num = `<span>${basic_number[i-1]}</span>`
-//     document.querySelector("section div #num").innerHTML = num;
-//   }
-//   let num1 = `<span>${bouns_number}</span>`
-//     document.querySelector("section #numberArea").innerHTML = num1;
-    let num = `<span style = "background-color: whitesmoke;
-   border-radius: 50%; height: 50px; width: 50px;">${basic_number[0]}</span>`
-    document.querySelector("section div #num1").innerHTML = num;
-    num = `<span style = "background-color: whitesmoke;
-   border-radius: 50%; height: 50px; width: 50px;">${basic_number[1]}</span>`
-    document.querySelector("section div #num2").innerHTML = num;
-    num = `<span style = "background-color: whitesmoke;
-   border-radius: 50%; height: 50px; width: 50px;">${basic_number[2]}</span>`
-    document.querySelector("section div #num3").innerHTML = num;
-    num = `<span style = "background-color: whitesmoke;
-   border-radius: 50%; height: 50px; width: 50px;">${basic_number[3]}</span>`
-    document.querySelector("section div #num4").innerHTML = num;
-    num = `<span style = "background-color: whitesmoke;
-   border-radius: 50%; height: 50px; width: 50px;">${basic_number[4]}</span>`
-    document.querySelector("section div #num5").innerHTML = num;
-    num = `<span style = "background-color: whitesmoke;
-   border-radius: 50%; height: 50px; width: 50px;">${basic_number[5]}</span>`
-    document.querySelector("section div #num6").innerHTML = num;
-    num = `<span style = "background-color: whitesmoke;
-   border-radius: 50%; height: 50px; width: 50px;">${bouns_number}</span>`
-    document.querySelector("section div #numbonus").innerHTML = num;
-  
-}
+  msg = lottos.toString().replaceAll(',', '');
+  numarea.innerHTML = msg;
+  // let basic_number = lottos.slice(0, -1).sort((a, b) => a - b);
+  // let bouns_number = lottos.slice(-1);
+  // let tags = '';
+  // basic_number.map((v) => {
+  //   tags = tags + `<span class = "sp${parseInt(v/10)}">${v}</span>`
+  // });
+  // tags = tags + `<span class = "plus">+</span><span class = "sp${parseInt(bouns_number/10)}">${bouns_number}</span>`
+  // numarea.innerHTML = tags;
+  // let tags = [];
+  // tags = lottos.map((v) => 
+  //   `<span class = "sp${parseInt(v / 10)}">${v}</span>`
+  // );
+  // tags.splice(6, 0,`<span class = "plus">+</span>`)
+  // msg = tags.toString().replaceAll(',', '');
+  // numarea.innerHTML = msg;
+  // console.log(msg);
+};
